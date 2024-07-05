f8549d281d650da61ec8da43605f54849117478c
mura123+ <mwsym1898@gmail.com>
2024-07-05 17:58:06 +0300

    add

--- Challenges/ConsoleApp1/ReverseWordsTest/UnitTest1.cs
+++ Challenges/ConsoleApp1/ReverseWordsTest/UnitTest1.cs
@@ -10,13 +10,19 @@ namespace ReverseWordsTest
         public void ReversalWordTest()
         {
             //Act 
-            string Sentence = "sentence this in words the Reverse";
+            string Sentence1 = "language programming is csharp";
+            string Sentence2 = "sentence this in words the Reverse";
+            string Sentence3 = "structures data and challenges";
 
             //Arrange
-            string ReverealWord  = Program.ReverseWords("Reverse the words in this sentence");
+            string ReverealWord1  = Program.ReverseWords("csharp is programming language");
+            string ReverealWord2  = Program.ReverseWords("Reverse the words in this sentence");
+            string ReverealWord3  = Program.ReverseWords("challenges and data structures");
 
             //Assert
-            Assert.Equal(Sentence, ReverealWord);
+            Assert.Equal(Sentence1, ReverealWord1);
+            Assert.Equal(Sentence2, ReverealWord2);
+            Assert.Equal(Sentence3, ReverealWord3);
         }
     }
 }
